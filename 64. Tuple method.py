b74b82f1149912a363dfbd6f21c199c74c208dc5
Krishna mourya <maurya1389@gmail.com>
2025-01-14 20:22:42 +0530

    UPDAATE

---
+++ 64. Tuple method.py
@@ -0,0 +1,26 @@
+print('---- Tuple Count Method ------')
+
+my_tuple = (1, 2, 3, 2, 'a', 'b', 'a')
+count_of_2 = my_tuple.count(2)
+count_of_a = my_tuple.count('a')
+
+print(count_of_2)  # Output: 2
+print(count_of_a)  # Output: 2
+
+print()
+
+#===================================================================================================
+
+print('---- tuple Index Method -----')
+
+my_tuple = (1, 2, 3, 'a', 'b', 'a')
+index_of_2 = my_tuple.index(2)
+index_of_a = my_tuple.index('a')
+
+print(index_of_2)  # Output: 1
+print(index_of_a)  # Output: 3
+
+print()
+
+
+#====================================================================================================
