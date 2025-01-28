7074a737c3d0240c8b1c6f72934c486d7cf8fe8b
Krishna mourya <maurya1389@gmail.com>
2025-01-28 19:06:41 +0530

    update

---
+++ Z/127.py
@@ -0,0 +1,10 @@
+# from package import *
+
+from Z import *
+print(addition.add(100,200))
+
+
+# If we try to access marvel function it will show error.
+# So we have add in __all__ special variable list
+
+# x.marvel()
